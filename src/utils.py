b1a33c6fbc0232485ec413f80e0fcd486df695c5
INRIX-Logan-Garwood <137229523+INRIX-Logan-Garwood@users.noreply.github.com>
2024-07-05 14:40:14 -0700

    add_trips_to_map

--- src/utils.py
+++ src/utils.py
@@ -22,11 +22,33 @@ def add_qks_to_map(map : folium.Map, qk_list):
         qk.show(map)
     return map
 
+def add_trips_to_map(map: folium.Map, trips_df: pd.DataFrame, color='blue'):
+    '''
+    Add trips to the map and return the new map
+    '''
+    for i in range(trips_df.shape[0]):
+        trip = trips_df.iloc[i]
+        folium.PolyLine(
+            locations=[[trip['start_lat'], trip['start_lon']], [trip['end_lat'], trip['end_lon']],
+                       [trip['end_lat'], trip['end_lon']]],
+            color=color,
+            weight=1,
+            opacity=0.8
+        ).add_to(map)
+    return map
+
 
 def main():
     qk_list = ['023112130', '023112131', '023112132']
     m = folium.Map(location=[47.6062, -122.3321], zoom_start=12)
     m = add_qks_to_map(m, qk_list)
+    trips = pd.DataFrame({
+        'start_lat': [47.6062, 47.6062],
+        'start_lon': [-122.3321, -122.3321],
+        'end_lat': [47.6062, 47.6062],
+        'end_lon': [-122.3321, -122.3321]
+    })
+    m = add_trips_to_map(m, trips)
 
 if __name__ == "__main__":
     main()
