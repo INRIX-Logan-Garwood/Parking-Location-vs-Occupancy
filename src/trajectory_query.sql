2f2286a7a14003f94a086f5fced20e1d8e7fcfa8
INRIX-Logan-Garwood <137229523+INRIX-Logan-Garwood@users.noreply.github.com>
2024-07-29 11:57:37 -0700

    trajectory update

--- src/trajectory_query.sql
+++ src/trajectory_query.sql
@@ -1,14 +1,14 @@
  SELECT trip.trip_id,
-trip.device_id,
-trip.end_qk,
-trip.provider_id,
-traj.traj_idx,
-traj.traj_raw_distance_m,
-traj.traj_raw_duration_millis,
-sol.point_idx,
-sol.utc_ts,
-sol.raw_lat,
-sol.raw_lon
+    trip.device_id,
+    trip.end_qk,
+    trip.provider_id,
+    traj.traj_idx,
+    traj.traj_raw_distance_m,
+    traj.traj_raw_duration_millis,
+    sol.point_idx,
+    sol.utc_ts,
+    sol.raw_lat,
+    sol.raw_lon
 FROM trajectories.trajectories trip
     CROSS JOIN UNNEST(trajectories) as t(traj)
     CROSS JOIN UNNEST(traj.solution_segments) as t(seg)
@@ -18,5 +18,8 @@ WHERE trip.map = 'osm'
     AND trip.region = 'na'
     AND trip.year in ('2023')
     AND trip.month in ('01')
-    AND trip.day in ('02','01')
-    AND trip.end_qk LIKE '023013202100232%'
+    AND trip.day in ('01', '02', '03', '04')
+    AND ((trip.end_qk LIKE '0230132021020101%') OR
+         (trip.end_qk LIKE '0230132021020110%') OR
+         (trip.end_qk LIKE '0230132021020100%') OR
+         (trip.end_qk LIKE '0230132021002323%'))
